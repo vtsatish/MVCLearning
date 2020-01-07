552f00d152dd8c68e037132aefe5f31a4ef75260
skumar1 <skumar1@vmware.com>
2020-01-07 19:54:46 +0530

    Initial working copy of Movies

---
+++ MVCLearning/Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCLearning.Models;
+
+namespace MVCLearning.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie/Analytics
+        public ActionResult Analytics()
+        {
+            var movieObject = new Movie() { MovieName = "Avengers", MovieId = 1 };
+            return View(movieObject);
+        }
+    }
+}
